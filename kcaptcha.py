94fa56c214b2d10b6de604918241f7509e2a5d0b
ods <none@none>
2011-03-29 18:51:52 +0400

    Use list as destination image data and put all them at once

--- kcaptcha.py
+++ kcaptcha.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from random import Random
-import string, math
+import string, math, itertools
 from PIL import Image, ImageFont, ImageDraw
 
 
@@ -21,8 +21,8 @@ class TextGenerator(object):
 class Captcha(object):
 
     mode = 'L'
-    bg_color = 'white'
-    color = 'black'
+    bg_color = 254
+    color = 0
 
     def __init__(self, size, font, random=None, **kwargs):
         self.size = size
@@ -41,7 +41,9 @@ class Captcha(object):
     def _amplitude(self):
         return self.random.uniform(3, 3.8)
 
-    def _wave(self, img, new_img):
+    def _wave(self, img):
+        dst_img = Image.new(self.mode, img.size, self.bg_color)
+        src_data = img.getdata()
         width, height = img.size
         dx_period_x = self._period()
         dx_period_y = self._period()
@@ -55,35 +57,35 @@ class Captcha(object):
         dy_amplitude = self._amplitude()
         # Variable lookup optimization
         sin = math.sin
-        get = img.getpixel
-        put = new_img.putpixel
-        for x in xrange(width):
-            for y in xrange(height):
-                # source x (float)
-                dx_x = sin(x * dx_period_x + dx_phase_x)
-                dx_y = sin(y * dx_period_y + dx_phase_y)
-                sx = x + (dx_x + dx_y) * dx_amplitude
-                if not 0 <= sx < width-1:
-                    continue
-                # source y (float)
-                dy_x = sin(x * dy_period_x + dy_phase_x)
-                dy_y = sin(y * dy_period_y + dy_phase_y)
-                sy = y + (dy_x + dy_y) * dy_amplitude
-                if not 0 <= sy < height-1:
-                    continue
-                sx_i = int(sx)
-                sy_i = int(sy)
-                frx = sx - sx_i
-                fry = sy - sy_i
-                # XXX This will work for L pallete only
-                color = int(
-                    get((sx_i, sy_i)) * (1-frx) * (1-fry) + \
-                    get((sx_i+1, sy_i)) * frx * (1-fry) + \
-                    get((sx_i, sy_i+1)) * (1-frx) * fry + \
-                    get((sx_i+1, sy_i+1)) * frx * fry
-                )
-                put((x, y), color)
-        return new_img
+        dst_data = [self.bg_color] * (height*width)
+        for x, y in itertools.product(xrange(width), xrange(height)):
+            # source x (float)
+            dx_x = sin(x * dx_period_x + dx_phase_x)
+            dx_y = sin(y * dx_period_y + dx_phase_y)
+            sx = x + (dx_x + dx_y) * dx_amplitude
+            if not 0 <= sx < width-1:
+                continue
+            # source y (float)
+            dy_x = sin(x * dy_period_x + dy_phase_x)
+            dy_y = sin(y * dy_period_y + dy_phase_y)
+            sy = y + (dy_x + dy_y) * dy_amplitude
+            if not 0 <= sy < height-1:
+                continue
+            sx_i = int(sx)
+            sy_i = int(sy)
+            frx = sx - sx_i
+            fry = sy - sy_i
+            idx1 = sx_i + width*sy_i
+            idx2 = idx1 + width
+            color = int(
+                src_data[idx1] * (1-frx) * (1-fry) + \
+                src_data[idx1+1] * frx * (1-fry) + \
+                src_data[idx2] * (1-frx) * fry + \
+                src_data[idx2+1] * frx * fry
+            )
+            dst_data[x + width*y] = color
+        dst_img.putdata(dst_data)
+        return dst_img
 
 
     def create(self, text):
@@ -93,9 +95,7 @@ class Captcha(object):
         left = int((self.size[0]-text_size[0])/2)
         top = int((self.size[1]-text_size[1])/2)
         draw.text((left, top), text, fill=self.color, font=self.font)
-        new_img = Image.new(self.mode, img.size, self.bg_color)
-        self._wave(img.getdata(), new_img.getdata())
-        return new_img
+        return self._wave(img)
 
 
 if __name__=='__main__':
@@ -104,7 +104,7 @@ if __name__=='__main__':
     captcha = Captcha(size=(120, 50), font=font)#, mode='RGB', color='#033')
     from time import time
     start = time()
-    count = 10
+    count = 20
     for i in range(count):
         text = get_text()
         print text
